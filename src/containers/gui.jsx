008f185afabb12d9f6245592498e402c4d0448f7
binghu <365356015@qq.com>
2020-05-04 09:04:19 +0800

    hello world

---
+++ src/containers/gui.jsx
@@ -0,0 +1,16 @@
+// import bindAll from 'lodash.bindall';
+import React from 'react';
+
+class Watermark extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div>dfdf</div>
+    );
+  }
+}
+
+export default Watermark;
